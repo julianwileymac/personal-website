924905e7a69ad7a8a25b0773115532958ce3afbb
julianwileymac <julianwileymac@gmail.com>
2020-09-02 18:49:19 -0400

    minor html changes

--- personal_profile/dashboard/admin.py
+++ personal_profile/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Order, RareMetalPrice, RareMetalSupplyAndDemand
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(RareMetalPrice)
+admin.site.register(RareMetalSupplyAndDemand)
